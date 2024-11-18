de73a7a37b2282c068f50c414f75e15cd54e8a5c
Shushant-Priyadarshi <profshushant@gmail.com>
2024-11-18 21:48:02 +0530

    tried solving vercel issue

--- client_job_portal/src/Landing/Companies.tsx
+++ client_job_portal/src/Landing/Companies.tsx
@@ -12,7 +12,7 @@ const Companies = () => {
     <div>
     <Marquee pauseOnHover={true}>
         {companies.map((company,index)=><div key={index} className="mx-8 px-2 py-1 mb-10 hover:bg-mine-shaft-950 rounded-lg cursor-pointer">
-            <img className="h-16 " src={`/assets/companies/${company}.png`} alt={`${company}-logo`}/>
+            <img className="h-16 " src={`../../public/assets/companies/${company}.png`} alt={`${company}-logo`}/>
         </div>)}
     </Marquee>
     </div>
